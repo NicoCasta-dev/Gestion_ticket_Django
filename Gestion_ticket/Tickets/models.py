f666594f9f00a37bc2fd4e92f4a8cb998969c8e8
Nicolas <nicocasta-dev@gmail.com>
2024-09-10 16:56:41 +0200

    no change

--- Gestion_ticket/Tickets/models.py
+++ Gestion_ticket/Tickets/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 import datetime
+from django.contrib.auth.models import User
+
 
 class Tickets_Ouverts(models.Model):
     titre = models.CharField(max_length=100)
     description = models.TextField()
-    date = models.DateTimeField()
+    date = models.DateField(default=datetime.datetime.now)
+    #auteur = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=20, choices=[('ouvert', 'Ouvert'), ('fermé', 'Fermé')], default='ouvert')
 
 
@@ -12,6 +15,7 @@ class Tickets_Ouverts(models.Model):
         return self.titre + ' ' + self.description + ' ' + self.date + ' ' + self.status
     
 class utilisateurs(models.Model):
+    utilisateur = models.OneToOneField(User, on_delete=models.CASCADE)
     nom = models.CharField(max_length=100)
     prenom = models.CharField(max_length=100)
     date = models.DateTimeField(default=datetime.datetime.now)
